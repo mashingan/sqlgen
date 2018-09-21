f4ffa1b587e91bc825305e5e84ca3021ffa0114e
mashingan <rahmat.d.ruffy@gmail.com>
2018-09-21 10:45:44 +0700

    make it a nimble package

---
+++ sqlgen.nim
@@ -0,0 +1,19 @@
+import sqlgen/[parsesql2, utils, types]
+import sqlgen/goout/[gotable, goentity]
+
+export parsesql2, utils, types, gotable, goentity
+
+when isMainModule:
+  proc main =
+    when defined(release):
+      var (filename, outpath) = parseCmd()
+      var tables = filename.parseSql.parse.getTables
+      var outfile: File
+      if outpath == "stdout" or outpath == "":
+        outfile = stdout
+      else:
+        outfile = open(outpath, fmWrite)
+      outfile.writeGoEntity(tables, needtime = tables.needtime)
+
+
+  main()
---
+++ sqlgen.nimble
@@ -0,0 +1,12 @@
+# Package
+
+version       = "0.1.0"
+author        = "Rahmatullah"
+description   = "SQL parser to create table object equivalent and Go output"
+license       = "MIT"
+srcDir        = "src"
+bin           = @["parsesql2"]
+
+# Dependencies
+
+requires "nim >= 0.18.0"
